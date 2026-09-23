2d83703b2d70235d11e8912e42cf5e9a31645a6f
Rawat_ji <167663545+ayushrawat1212@users.noreply.github.com>
2026-09-23 19:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+//Step 1: Target the elements in the DOM
+const removeBtn = document.querySelector("input[type="button"]");
+const colorDropDown = document.getElementById("colorSelect");
+
+//Step 2 & 3: Define the removal logic func
+function removeSelectedColor() {
+	
+	//check if a valid item is actually selected
+	if(colorDropDown.selectedIndex !== -1) {
+		//remove the item
+		colorDropDown.remove(colorDropDown.selectedIndex);
+	}
+}
+
+//Step 4: Bind the func to execute on a click event 
+removeBtn.addEventListener("click", removeSelectedColor);
+
